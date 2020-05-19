b9e3ceaf27b7f0b39734da0bdc6f057d6ba22975
DetegiCE <martinok1103@naver.com>
2020-05-19 20:11:09 +0900

    mp4 update

--- getpics.py
+++ getpics.py
@@ -1,8 +1,7 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
 import io
 import re
 import urllib.request
+from bs4 import BeautifulSoup
 
 def getUrl(file):
     f = open(file, "r")
@@ -14,11 +13,20 @@ def getUrl(file):
     p = re.compile("small_slide_\([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\)\.png")
     res1 = p.findall(atxt)
     reslen = len(res1)
+    print("Pictures Downloading...")
     for i in range(0, reslen//2):
         tpic = res1[i]
         tpic = tpic.replace("small_slide","large_slide")
         tpic = tpic.replace(".png",".jpg")
         turl = imus[0] + "/slide/" + tpic
-        print(turl)
         urllib.request.urlretrieve(turl,"./download/p"+str(i+1)+".jpg")
-        print("OK "+str(i))
+        print(str(i+1)+"/"+str(reslen//2)+" Complete")
+    print("Pictures Downloading complete")
+    print("MP4 Downloading...",end='')
+    q = re.compile("main_\([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\)\.mp4")
+    res2 = q.findall(atxt)
+    aurl = imus[0].replace("web_files","media_files") + "/" + res2[0]
+    urllib.request.urlretrieve(aurl, "./download/audio.mp4")
+    print("done")
+
+
