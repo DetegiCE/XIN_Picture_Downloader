8db579526742887ec305be05c007df0094daa831
DetegiCE <martinok1103@naver.com>
2020-05-26 15:56:17 +0900

    add PDF conversion

--- getpics.py
+++ getpics.py
@@ -2,6 +2,7 @@ import io
 import re
 import urllib.request
 from bs4 import BeautifulSoup
+from PIL import Image
 
 def getUrl(file):
     f = open(file, "r")
@@ -14,6 +15,7 @@ def getUrl(file):
     res1 = p.findall(atxt)
     reslen = len(res1)
     print("Pictures Downloading...")
+    imgs = list()
     for i in range(0, reslen//2):
         tpic = res1[i]
         tpic = tpic.replace("small_slide","large_slide")
@@ -21,12 +23,18 @@ def getUrl(file):
         turl = imus[0] + "/slide/" + tpic
         urllib.request.urlretrieve(turl,"./download/p"+str(i+1)+".jpg")
         print(str(i+1)+"/"+str(reslen//2)+" Complete")
-    print("Pictures Downloading complete")
+        if i == 0:
+            img1 = Image.open("./download/p1.jpg")
+            im1 = img1.convert("RGB")
+        else:
+            imgX = Image.open("./download/p"+str(i+1)+".jpg")
+            imX = imgX.convert("RGB")
+            imgs.append(imX)
+    im1.save("./download/slide.pdf", save_all=True, append_images=imgs)
+    print("Pictures Download & PDf conversion complete")
     print("MP4 Downloading...",end='')
     q = re.compile("main_\([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\)\.mp4")
     res2 = q.findall(atxt)
     aurl = imus[0].replace("web_files","media_files") + "/" + res2[0]
     urllib.request.urlretrieve(aurl, "./download/audio.mp4")
     print("done")
-
-
